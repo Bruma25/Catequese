31147e50b30865e1583a5fd59fa03cf89f258888
bruma25 <bruma.gm25@gmail.com>
2026-09-19 00:08:54 -0300

    fix(ajuste rotas de página Meus Catequizandos)

--- app/main.py
+++ app/main.py
@@ -21,7 +21,6 @@ app.add_middleware(
 
 app.include_router(inscricao_router, prefix="/api/v1")
 app.include_router(usuario_router, prefix="/api/v1")
-app.include_router(catequizando_router, prefix="/api/v1")
 
 @app.get("/")
 def root():
